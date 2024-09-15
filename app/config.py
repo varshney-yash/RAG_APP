151d02fb2a5cfa4ec12365ac03733f2bd6bd6712
varshney-yash <107148830+varshney-yash@users.noreply.github.com>
2024-09-15 18:14:46 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "ff75829f-b02f-44ce-b70b-b6436c0ee669")
-PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "qa-index")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyCGBrBX01PIMIraaxbtMhC9JJjT5J8ddmc")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
 
